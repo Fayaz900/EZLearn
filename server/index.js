41ac6ab38189028560e08c7245b294df05871d4c
Fayaz Azeez <fayazazeez2@gmail.com>
2024-09-16 08:15:27 +0530

    Db connected , server setup done

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 import express from "express"
+import dotenv from 'dotenv'
+import { connectDB } from "./config/db.js";
+
+dotenv.config()
 
 const app = express()
-const port = 5000
+const port = process.env.PORT;
+
+app.get('/',(req,res)=>{
+    res.send("Server is Working")
+})
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
-    
+    connectDB()
 })
