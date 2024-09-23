6c54b531f3b0fd573f17eb39487fda773f01ec25
Fayaz Azeez <fayazazeez2@gmail.com>
2024-09-23 10:54:59 +0530

    login intiated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import HomePage from './components/pages/home/HomePage'
+import Header from './components/header/Header'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
