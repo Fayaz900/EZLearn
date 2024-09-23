91a473552f383567afcef231487a147e866e3ab5
Fayaz Azeez <fayazazeez2@gmail.com>
2024-09-23 11:21:52 +0530

    basic design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import HomePage from './components/pages/home/HomePage'
 import Header from './components/header/Header'
+import Login from './components/AuthComponents/Login/Login'
+import RegistartionForm from './components/AuthComponents/Register/Register'
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<RegistartionForm/>}/>
       </Routes>
       </BrowserRouter>
     </div>
